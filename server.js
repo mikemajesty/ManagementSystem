6975167f00bcf1c9e4fba885f7fd7270ab71cd76
mikemajesty <mikemajesty2011@gmail.com>
2016-05-19 20:44:32 -0300

    Adds a angular to index page

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const routes = require('./server/router/index');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/server',express.static(`${__dirname}/server`));
+app.use('/server',express.static(path.join(__dirname, 'server')));
 // app.use('/www',express.static(`${__dirname}/www`));
-// app.use('/node_modules',express.static(`${__dirname}/node_modules`));
+ app.use('/node_modules',express.static(path.join(__dirname, '/node_modules')));
 
 app.use('/', routes);
 

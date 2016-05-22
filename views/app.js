990992811e3a8aae5fe156be586b460b5452dcdd
mikemajesty <mikemajesty2011@gmail.com>
2016-05-22 17:58:49 -0300

    Remove app.js commentary

--- views/app.js
+++ views/app.js
@@ -12,23 +12,5 @@
         controller: 'CategoryController'
       });
 
-              //
-              // $urlRouterProvider.otherwise('/home');
-              //
-              // $stateProvider.state('signUp', {
-              //   url: '/signup',
-              //   templateUrl: 'app/signup/signup.html',
-              //   controller: 'SignUpController'
-              // })
-              // .state('editProfile', {
-              //   url: '/edit-profile',
-              //   templateUrl: 'app/profile/edit-profile-view.html',
-              //   controller: 'EditProfileController'
-              // })
-              // .state('main', {
-              //   url: '/home',
-              //   templateUrl: 'app/main/main.html',
-              //   controller: 'MainController'
-              // });
-          });
+    });
 }());

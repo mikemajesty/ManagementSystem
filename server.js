687079c137bd7ec5ac2a03d8a61ee5dabf83b481
mikemajesty <mikemajesty2011@gmail.com>
2016-05-27 15:26:11 -0300

    Removing unnecessary files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'jade');
 
 app.use('/public', express.static(path.join(__dirname, 'www')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use('/views', express.static(path.join(__dirname, 'views')));
+//app.use('/views', express.static(path.join(__dirname, 'views')));
 app.use('/', routes);
 
 app.listen(3000, () => {

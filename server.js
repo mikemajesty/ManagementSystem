c64ee2d08023ca5b03dea3a9ede7d21d8b3c35d8
mikemajesty <mikemajesty2011@gmail.com>
2016-05-27 12:41:36 -0300

    Change libs path

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const routes = require('./server/router/index');
 app.set('view engine', 'jade');
 
 app.use('/public', express.static(path.join(__dirname, 'www')));
-app.use('/teste', express.static(path.join(__dirname, 'www/html/menu')));
+app.use('/libs', express.static(path.join(__dirname, 'node_modules')));
+
 app.use('/', routes);
 
-app.listen(3000, (a) => {
+app.listen(3000, () => {
   console.log('Running in port 3000');
 });

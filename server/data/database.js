3dcb747a13539289b3da7b523c139ba06ad7c0b5
mikemajesty <mikemajesty2011@gmail.com>
2016-05-28 10:24:07 -0300

    Cratea a connection in mysql

---
+++ server/data/database.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('myapp', 'roor', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});

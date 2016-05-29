85329ff75b25c48496345addc86aef63b439aa9d
mikemajesty <mikemajesty2011@gmail.com>
2016-05-29 18:23:12 -0300

    Refactoring controllers

--- views/category/category-controller.js
+++ views/category/category-controller.js
@@ -1,21 +1,21 @@
-(function () {
-  angular.module('myapp')
-    .controller('CategoryController', ['$scope', '$http', function($scope, $http) {
+( (angular) => {
+  'use strict';
+  angular.module('myapp').controller('CategoryController', ['$scope', '$http',
+      ($scope, $http) => {
+        $scope.getCategory = () => {
+          $http.get('api/category/findAll').then( (data) => {
+            $scope.categories = data;
+          }).catch((err) => {console.log('opa parça', err);});
+        };
 
-      $scope.getCategory = () => {
-         $http.get('api/category/findAll').then( (data) => {
-           $scope.categories = data;
-         }).catch((err) => {console.log('opa parça', err);});
-      };
-
-      $scope.create = () => {
-         let category = {
-           name: $scope.name || null,
-           descripition: $scope.descripition || null
-         }
-         $http.post('api/category/create', category).then( (data) => {
-           console.log('created', data);
-         }).catch((err) => {console.log('opa parça', err);});
-      };
+        $scope.create = () => {
+           let category = {
+             name: $scope.name || null,
+             descripition: $scope.descripition || null
+           }
+           $http.post('api/category/create', category).then( (data) => {
+             console.log('created', data);
+           }).catch((err) => {console.log('opa parça', err);});
+        };
     }]);
-}());
+})(angular);
--- views/home/index-controller.js
+++ views/home/index-controller.js
@@ -1,21 +1,22 @@
-(function() {
-    angular.module('myapp').controller('HomeController',['$scope', '$http',
-     function ($scope, $http) {
+((angular) => {
+  'use strict';
+  angular.module('myapp').controller('HomeController',['$scope', '$http',
+     ($scope, $http) => {
 
-       $scope.getCategory = () => {
-          $http.get('api/category/findAll').then( (data) => {
-            $scope.categories = data.data;
-          }).catch((err) => {console.log('opa parça', err);});
-       };
+     $scope.getCategory = () => {
+        $http.get('api/category/findAll').then( (data) => {
+          $scope.categories = data.data;
+        }).catch((err) => {console.log('opa parça', err);});
+     };
 
-       $scope.create = () => {
-          let category = {
-            name: $scope.name || null,
-            descripition: $scope.descripition || null
-          }
-          $http.post('api/category/create', category).then( (data) => {
-            console.log('created', data);
-          }).catch((err) => {console.log('opa parça', err);});
-       };
-    }]);
-}());
+     $scope.create = () => {
+        let category = {
+          name: $scope.name || null,
+          descripition: $scope.descripition || null
+        }
+        $http.post('api/category/create', category).then( (data) => {
+          console.log('created', data);
+        }).catch((err) => {console.log('opa parça', err);});
+     };
+  }]);
+})(angular);

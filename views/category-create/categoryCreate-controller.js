3fcaa3a0abbfef37e0cccd53494259e86fac9a63
mikemajesty <mikemajesty2011@gmail.com>
2016-06-04 23:26:15 -0300

    Adds form save category

--- views/category-create/categoryCreate-controller.js
+++ views/category-create/categoryCreate-controller.js
@@ -1,12 +1,23 @@
 ((angular) => {
   'use sctrict'
   angular.module('myapp')
-    .controller('CategoryCreateController', ['$scope', controller]);
+    .controller('CategoryCreateController', ['$scope', '$http', controller]);
 
-     function controller($scope) {
+     function controller($scope, $http) {
 
-      
-
+       $scope.submit = () => {
+         const data = {
+           name: $scope.category.name,
+           descripition: $scope.category.descripition
+         };
+         console.log(data);
+         $http.post('api/category/create', data)
+          .then((data) => {
+            console.log('was saved');
+          }).catch((err) => {
+            console.log('was error');
+          });
+       };
 
 
 

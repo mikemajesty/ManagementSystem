46d2a57b92ba8111b31ba9ab6b02772750cde93e
mikemajesty <mikemajesty2011@gmail.com>
2016-06-05 12:31:18 -0300

    Adds category edit

--- server/data/categoryRepository.js
+++ server/data/categoryRepository.js
@@ -19,13 +19,29 @@ const remove = (id) => {
   return Category.Category.destroy({ where: { id: id }});
 }
 
-const edit = (id) => {
-    return Category.Category.all();
+const edit = (category) => {
+    return Category.Category.find(category.id)
+      .then((data) => {
+        data.updateAttributes({
+          name: category.name,
+          descripition: category.descripition
+        }).then((data) => {
+            console.log('was update:', data);
+        }).catch((err) => {
+          console.log('error:', err);
+        });
+      });
+};
+
+const getBy = (id) => {
+  return Category.Category.find({id: id});
+
 };
 
 module.exports =  {
    create,
    getAll,
    remove,
-   edit
+   edit,
+   getBy
 };
